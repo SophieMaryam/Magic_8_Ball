09462e25c7bc6f4568568f5b4166621f004d0f1f
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2018-04-05 16:29:37 +0200

    Image changes when button touched

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,21 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    var randomBallNumber = 0
+  
     @IBOutlet weak var imageView: UIImageView!
-    @IBAction func askButtonPressed(_ sender: UIButton) {
-    }
-   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateBallImage()
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        updateBallImage()
+    }
+    
+    func updateBallImage(){
+        randomBallNumber = Int(arc4random_uniform(4))
+        imageView.image = UIImage(named: ballArray[randomBallNumber])
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+    }
 
 }
 
